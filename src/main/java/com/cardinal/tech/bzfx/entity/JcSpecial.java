79cfb847e128dada625d3d3f042171716f86e826
zc <zcsoft@icloud.com>
2021-12-02 22:20:43 +0800

    专项任务表，增加数据同步状态字段；

--- src/main/java/com/cardinal/tech/bzfx/entity/JcSpecial.java
+++ src/main/java/com/cardinal/tech/bzfx/entity/JcSpecial.java
@@ -9,10 +9,10 @@ import lombok.Data;
  * 专项任务(JcSpecial)实体类
  *
  * @author makejava
- * @since 2021-11-20 18:42:52
+ * @since 2021-12-02 22:15:38
  */
 public class JcSpecial implements Serializable {
-    private static final long serialVersionUID = 529801541387953123L;
+    private static final long serialVersionUID = -98502523896087374L;
     /**
     * id
     */
@@ -25,6 +25,14 @@ public class JcSpecial implements Serializable {
     * 描述
     */
     private String description;
+    /**
+    * 统计状态[0-未统计，1-统计中，2-已统计]
+    */
+    private Integer tjState;
+    /**
+    * 最后统计完成的时间
+    */
+    private Date tjDate;
     /**
     * 数据创建时间
     */
